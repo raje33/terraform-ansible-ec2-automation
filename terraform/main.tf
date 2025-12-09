3b1fe0d00f3500cea0ead7c50250e06f4ee66c8c
raje33 <rajeswari.manick@outlook.com>
2025-12-09 15:40:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,30 @@
-provider "aws" {
-  region = "ap-south-1"
+data "aws_ssm_parameter" "ubuntu_2204" {
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
+}
+
+resource "aws_key_pair" "tfkey" { key_name = "tf-key"; public_key = file(var.ssh_public_key_path) }
+
+resource "aws_security_group" "main_sg" {
+  name = "tf-ansible-sg"
+  ingress { from_port=22; to_port=22; protocol="tcp"; cidr_blocks=["0.0.0.0/0"] }
+  ingress { from_port=80; to_port=80; protocol="tcp"; cidr_blocks=["0.0.0.0/0"] }
+  egress  { from_port=0; to_port=0; protocol="-1"; cidr_blocks=["0.0.0.0/0"] }
 }
 
 resource "aws_instance" "controller" {
-  ami           = "ami-0fd05997b4dff7aac"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "controller-node"
-  }
+  ami = data.aws_ssm_parameter.ubuntu_2204.value
+  instance_type = var.instance_type
+  key_name = aws_key_pair.tfkey.key_name
+  vpc_security_group_ids = [aws_security_group.main_sg.id]
+  associate_public_ip_address = true
+  tags = { Name = "controller" }
 }
 
 resource "aws_instance" "managed" {
-  ami           = "ami-0fd05997b4dff7aac"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "managed-node"
-  }
+  ami = data.aws_ssm_parameter.ubuntu_2204.value
+  instance_type = var.instance_type
+  key_name = aws_key_pair.tfkey.key_name
+  vpc_security_group_ids = [aws_security_group.main_sg.id]
+  associate_public_ip_address = true
+  tags = { Name = "managed" }
 }
