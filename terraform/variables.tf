d1b63347a3173e111ac26010327d3c8ae0a7c945
raje33 <rajeswari.manick@outlook.com>
2025-12-09 15:39:10 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,4 @@
-# No variables used in this simple example
+variable "aws_region" { default = "us-east-1" }
+variable "instance_type" { default = "t2.micro" }
+variable "ssh_public_key_path" { default = "~/.ssh/tf_key.pub" }
+
